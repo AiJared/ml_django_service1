e97b2775b5e5dab496879fc86a754ad32185f22d
Kerriea-star <ojared511@gmail.com>
2023-02-25 11:56:10 +0300

    ABTest and StopABTest urls

--- backend/server/apps/endpoints/urls.py
+++ backend/server/apps/endpoints/urls.py
@@ -6,6 +6,8 @@ from apps.endpoints.views import MLAlgorithmViewSet
 from apps.endpoints.views import MLAlgorithmStatusViewSet
 from apps.endpoints.views import MLRequestViewSet
 from apps.endpoints.views import PredictView # import predict view
+from apps.endpoints.views import ABTestViewSet
+from apps.endpoints.views import StopABTestView
 
 router = DefaultRouter(trailing_slash=False)
 
@@ -13,9 +15,11 @@ router.register(r"endpoints", EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
 router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatus")
 router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+router.register(r"abtests", ABTestViewSet, basename="abtests")
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
     # add predict view url
     path("api/v1/<str:endpoint_name>/predict", PredictView.as_view(), name="predict"),
+    path("api/v1/stop_ab_test/<str:ab_test_id>", StopABTestView.as_view(), name="stop_ab"),
 ]
